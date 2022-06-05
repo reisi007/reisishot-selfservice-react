89fd0a0e19098989f64fed3bc229cf3709bbaabb
Florian Reisinger <reisi007@gmail.com>
2022-06-05 20:33:44 +0200

    Basic route setup for waitlist | disable animation for review page

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,34 @@
 import { Suspense } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter as Router, Navigate, Route, Routes,
+} from 'react-router-dom';
 
 import { configure } from 'axios-hooks';
 import LRU from 'lru-cache';
 import Root from './Root';
 import { useAdminRoutes } from './admin/routes';
+import { useWaitlistRoutes } from './waitlist/routes';
 
 // Configure useAxiosCache
-configure({ cache: new LRU({ ttl: 60_000, max: 1_000 }) });
+configure({
+  cache: new LRU({
+    ttl: 60_000,
+    max: 1_000,
+  }),
+});
 
 function App() {
   const adminRoutes = useAdminRoutes();
+  const waitlistRoutes = useWaitlistRoutes();
   return (
     <div className="container w-full h-full">
       <Router>
         <Suspense fallback={<div />}>
           <Routes>
             <Route index element={<Root />} />
+            {waitlistRoutes}
             {adminRoutes}
+            <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         </Suspense>
       </Router>
---
+++ src/waitlist/Waitlist.module.ts
@@ -0,0 +1,7 @@
+import { Waitlist as PrivateWaitlist } from './private/Waitlist';
+import { Waitlist as PublicWaitlist } from './public/Waitlist';
+import { EmailLanding } from './private/EmailLanding';
+
+export const EmailLandingPage = EmailLanding;
+export const PrivateWaitlistPage = PrivateWaitlist;
+export const PublicWaitlistPage = PublicWaitlist;
