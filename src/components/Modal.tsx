7dcec6bf2a3091e524b1f5388c1aeee6b2652c4e
Florian Reisinger <reisi007@gmail.com>
2022-06-17 20:16:44 +0200

    Fix dialog styling on specific screen sizes

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -21,7 +21,7 @@ function Modal({
     >
       <dialog
         onClick={(e) => e.stopPropagation()}
-        className="block top-[10%] left-1/2 z-50 w-full rounded-lg shadow-lg -translate-x-1/2 sm:w-3/4 sm:h-1/2  md:w-1/2"
+        className="block top-[10%] left-1/2 z-50 w-full rounded-lg shadow-lg -translate-x-1/2 sm:w-3/4 md:w-1/2"
       >
         <h2 className="mb-2">{title}</h2>
         <div className="overflow-x-hidden overflow-y-auto max-h-[66.67vh]">
