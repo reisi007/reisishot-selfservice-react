e4186c7250837c4e7d9a9d0e3ea8a7c510d8c2ad
Florian Reisinger <reisi007@gmail.com>
2022-06-21 19:39:37 +0200

    Minor improvements in admin UI

--- src/admin/support/PendingSignatureTab.tsx
+++ src/admin/support/PendingSignatureTab.tsx
@@ -15,31 +15,36 @@ export function PendingSignatureTab({ data: loginData }: TabProps<LoginData>) {
     <>
       <h2>{t('admin.waitlist.support.tabs.pendingSignatures.title')}</h2>
       <Loadable {...request} loadingElement={<LoadingIndicator />}>
-        {(response) => response.pendingContracts.length > 0 && (
-          <div className="grid gap-2 m-2 md:grid-cols-2">
-            {response.pendingContracts.map(({
-              due_date: dueDate,
-              email,
-              access_key: accessKey,
-            }) => {
-              const label = t('contracts.display.title', {
-                dateTime: formatDateTime(dueDate),
-                name: email,
-              });
-              const key = email + accessKey + dueDate;
-              const url = computeContractLink({
-                user: email,
-                auth: accessKey,
-              });
-              return (
-                <StyledCopyLinkButton key={key} href={url}>
-                  <p>
-                    {label}
-                  </p>
-                </StyledCopyLinkButton>
-              );
-            })}
-          </div>
+        {(response) => (
+          <>
+            {response.pendingContracts.length > 0 && (
+            <div className="grid gap-2 m-2 md:grid-cols-2">
+              {response.pendingContracts.map(({
+                due_date: dueDate,
+                email,
+                access_key: accessKey,
+              }) => {
+                const label = t('contracts.display.title', {
+                  dateTime: formatDateTime(dueDate),
+                  name: email,
+                });
+                const key = email + accessKey + dueDate;
+                const url = computeContractLink({
+                  user: email,
+                  auth: accessKey,
+                });
+                return (
+                  <StyledCopyLinkButton key={key} href={url}>
+                    <p>
+                      {label}
+                    </p>
+                  </StyledCopyLinkButton>
+                );
+              })}
+            </div>
+            )}
+            {response.pendingContracts.length === 0 && t('admin.waitlist.support.tabs.pendingSignatures.done')}
+          </>
         )}
       </Loadable>
     </>
