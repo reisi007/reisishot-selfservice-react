0891b1894fd707fe27430346fe515177933ebe51
Florian Reisinger <reisi007@gmail.com>
2022-09-03 20:12:41 +0200

    Direct link to choose image page

--- src/waitlist/private/EmailChooseLanding.tsx
+++ src/waitlist/private/EmailChooseLanding.tsx
@@ -1,8 +1,33 @@
-import { Navigate, useParams } from 'react-router-dom';
+import { Navigate, useNavigate, useParams } from 'react-router-dom';
+import { useEffect } from 'react';
 import { useWaitlistLogin } from '../WaitlistContextProvider';
+import { LoadingIndicator } from '../../LoadingIndicator';
+
+function WaitForRedirect({
+  email,
+  hash,
+  chooseId,
+}: { email: string, hash: string, chooseId: string }) {
+  const [loginData, setLoginData] = useWaitlistLogin();
+  const navigate = useNavigate();
+  setLoginData({
+    user: email,
+    auth: hash,
+  });
+  useEffect(() => {
+    if (loginData === undefined) return;
+    const {
+      user,
+      auth,
+    } = loginData;
+    if (user === email && hash === auth) {
+      navigate(`/waitlist/choose/${chooseId}`);
+    }
+  }, [chooseId, email, hash, loginData, navigate]);
+  return <LoadingIndicator />;
+}
 
 export function EmaiChooselLanding() {
-  const [, setLoginData] = useWaitlistLogin();
   const {
     email,
     hash,
@@ -11,9 +36,5 @@ export function EmaiChooselLanding() {
   if (email === undefined || hash === undefined || chooseId === undefined) {
     return <Navigate to="/waitlist" />;
   }
-  setLoginData({
-    user: email,
-    auth: hash,
-  });
-  return <Navigate to={`/waitlist/choose/${chooseId}`} />;
+  return <WaitForRedirect email={email} hash={hash} chooseId={chooseId} />;
 }
